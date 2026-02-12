fb5921279ef2bc7c8e824eeebf6c3d82e11c2f11
Rhythm Sharma <rhysha2805@gmail.com>
2026-02-12 17:44:42 +0530

    added the toggle and sound feature to pomodoro

--- src/components/desk-components/pomodoro/pomodoro.jsx
+++ src/components/desk-components/pomodoro/pomodoro.jsx
@@ -1,66 +1,96 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { TimerDisplay } from "./timeDisplay";
 import { Controls } from "./controls";
 
-export function Pomodoro(){
-    const timers={
-    shortBreak:300,
-    longBreak:900,
-    study:1500
-}
-    
-    const [mode,setMode]=useState("shortBreak"); // shortbreak || longbreak
-    const [timeLeft,setTimeLeft]=useState(timers.shortBreak); //to update timer
-    const [isPaused,setIsPaused]=useState(false); // see if timer paused
-    const [sessions,setSessions]=useState(0); //count number of sessions
-
-    useEffect(()=>{
-        if(isPaused)return;
-        if(timeLeft<=0) return 0;
-        const interval=setInterval(()=>{
-        setTimeLeft(prev=>prev-1)},1000);
-
-        return ()=>clearInterval(interval);
-    },[isPaused,timeLeft]);
-
-
-    useEffect(() => {
-  if (timeLeft !== 0) return;
-
-  if (mode === "study") {
-    setSessions(prev => prev + 1);
-    setMode("shortBreak");
-    setTimeLeft(timers.shortBreak);
-  } else if(sessions==4){
-    setSessions(0);
-    setMode("longBreak");
-    setTimeLeft(timers.longBreak);
-  }else  {
-    setMode("study");
-    setTimeLeft(timers.study);
+export function Pomodoro() {
+  const timers = {
+    shortBreak: 15,
+    longBreak: 900,
+    study: 10
   }
-}, [timeLeft]);
 
-const reset=()=>{
-  setTimeLeft(timers[mode]);
-  setIsPaused(true)
-}
-const start=()=>{
-  if(isPaused===true) setIsPaused(false);
-  else{setIsPaused(true);}
-  console.log(isPaused);
-}
+  const [mode, setMode] = useState("study"); // shortbreak || longbreak
+  const [timeLeft, setTimeLeft] = useState(timers.study); //to update timer
+  const [isPaused, setIsPaused] = useState(false); // see if timer paused
+  const [pomodoro, setPomodoros] = useState(0); //count number of sessions
+  const [isStarted, setIsStarted] = useState(false);
+  const [sound,setSound]=useState(true);
 
-    return(
-        <>
-        <div className="min-w-80 rounded-xl bg-emerald-600 p-4">
-          
-            <TimerDisplay timeLeft={timeLeft} mode={mode}/>
-            <Controls onReset={reset} onStart={start} />
-        </div>
-            
-        </>
-    )
+  const audioRef=useRef(null);
+
+  useEffect(() => {
+    if (!isStarted || isPaused || timeLeft <= 0) return;
+
+    const interval = setInterval(() => {
+      setTimeLeft(prev => prev - 1)
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [isPaused, isStarted, timeLeft]);
+
+
+  useEffect(() => {
+    if (timeLeft !== 0) return;
+
+    playSound();
+    if (mode === "study") {
+      setPomodoros(prev => {
+        const next = prev + 1;
+
+        if (next === 4) {
+          setMode("longBreak");
+          setTimeLeft(timers.longBreak);
+          return 0;
+        }
+
+        setMode("shortBreak");
+        setTimeLeft(timers.shortBreak);
+        return next;
+      });
+    }else{
+      setMode("study");
+      setTimeLeft(timers.study)
+    }
+  }
+, [timeLeft]);
+
+  const playSound=()=>{
+      if(!sound) return;
+      const audio=audioRef;
+      audio.current.play();
+  }
+
+
+  const reset = () => {
+    setIsStarted(false);
+    setTimeLeft(timers[mode]);
+    setIsPaused(true)
+  }
+  // 
+
+  const start = () => {
+    if (!isStarted) {
+      setIsStarted(true);
+      setIsPaused(false);
+    } else {
+      setIsPaused(prev => !prev);
+    }
+  };
+
+  const onSound=()=>{
+    setSound(!sound);
+  }
+
+  return (
+    <>
+      <div className="min-w-80 rounded-xl bg-emerald-600 p-4 " >
+        
+        <TimerDisplay timeLeft={timeLeft} mode={mode} pomodoro={pomodoro} audioRef={audioRef} />
+        <Controls onReset={reset} onStart={start} onSound={onSound} isStarted={isStarted} isPaused={isPaused} sound={sound} />
+      </div>
+
+    </>
+  )
 }
