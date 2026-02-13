9e7f58bd864e92c0dccfe6d6172ca731958b0908
aievey <amitabh283verma@gmail.com>
2026-02-13 10:49:55 +0530

    last commit on feb13. Add login and prepare home next

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,13 @@
+function Login () {
+  return (
+  <form>
+    <input placeholder="username" type="text"/>
+    <input type="password" placeholder="password" />
+    <button>Login</button> <button>Make new account</button>
+  </form>
+  );
+}
+
 export const metadata = {
   title: "Sleep on it!",
   description: "The best productive learning app on the market",
@@ -6,6 +16,8 @@ export default function Home() {
   return (
     <main>
       <div>Hello world!</div>
+      <Login />
+
     </main>
   );
 }
